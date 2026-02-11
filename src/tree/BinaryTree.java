699af3d6b740aa3f9468c29edb273f8935bb4aab
Dago Palmera <dagopalmera1@gmail.com>
2026-02-11 10:50:29 -0500

    Implement insert method and recursive insertion logic in BinaryTree class

--- src/tree/BinaryTree.java
+++ src/tree/BinaryTree.java
@@ -1,5 +1,24 @@
 package tree;
 
 public class BinaryTree {
-    
+
+    public BinaryNode root;
+
+    public void insert(int data) {
+        root = insertRec(root, data);
+    }
+
+    private BinaryNode insertRec(BinaryNode node, int data) {
+        if (node == null) {
+            return new BinaryNode(data);
+        }
+
+        if (data < node.data) {
+            node.left = insertRec(node.left, data);
+        } else if (data > node.data) {
+            node.right = insertRec(node.right, data);
+        }
+
+        return node;
+    }
 }
